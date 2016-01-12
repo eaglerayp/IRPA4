c42e9c8572d0a4d00748f627d58344e70dddd9a0
eaglerayp <eagle_rayp@hotmail.com>
2016-01-12 18:57:31 +0800

    add version before testing

--- pa4.go
+++ pa4.go
@@ -41,23 +41,10 @@ func CosineSimilarity(doc1 map[int]float64, doc2 map[int]float64) float64 {
 	return Similarity
 }
 
-func showCluster(doc_cluster [1095]int) {
-	ClusterDocsMap := make(map[int][]int)
-	for index := 0; index < 1095; index++ {
-		// if no this cluster, create slice
-		if _, val := ClusterDocsMap[doc_cluster[index]]; val {
-			list := []int{index} //array initialize
-			ClusterDocsMap[doc_cluster[index]] = list
-		} else {
-			// else append
-			ClusterDocsMap[doc_cluster[index]] = append(ClusterDocsMap[doc_cluster[index]], index)
-		}
-	}
-
-	// display
-	for _, list := range ClusterDocsMap {
+func showCluster(cluster_doc map[int][]int) {
+	for _, list := range cluster_doc {
 		for doc := range list {
-			fmt.Println(doc + 1)
+			fmt.Println(list[doc] + 1)
 		}
 		fmt.Println()
 	}
@@ -72,10 +59,13 @@ func main() {
 	var SimArray []float64
 	simIndex := 0
 	SimMap := make(map[float64][]int)
+	var Cluster_SimDoubleArray [1095][1095]float64
 	// below doc always = index, means 1.txt = doc 0
 	for doc1 := 0; doc1 < docnum; doc1++ {
 		for doc2 := doc1 + 1; doc2 < docnum; doc2++ {
 			sim := CosineSimilarity(doclist[doc1], doclist[doc2])
+			Cluster_SimDoubleArray[doc1][doc2] = sim
+			Cluster_SimDoubleArray[doc2][doc1] = sim
 			SimArray = append(SimArray, sim)
 			simIndex++
 			array := []int{doc1, doc2}
@@ -84,9 +74,15 @@ func main() {
 	}
 	// initial doc-cluster map, doc 0 belongs to cluster 0
 	var doc_cluster [1095]int
+	// initial cluster-doc map, cluster 0 has doc 0
+	cluster_doc := make(map[int][]int)
 	for index := 0; index < 1095; index++ {
 		doc_cluster[index] = index
+		array := []int{index}
+		cluster_doc[index] = array
+		fmt.Println(index)
 	}
+
 	sort.Float64s(SimArray)
 	// small to big
 	// single link,
@@ -95,12 +91,20 @@ func main() {
 		// // max cosine~
 		doc1 := SimMap[SimArray[index]][0]
 		doc2 := SimMap[SimArray[index]][1]
-		// //merge
-		// if doc1 != doc2 {
-		// 	cluster_count--
-		// 	doc_cluster[doc2] = doc_cluster[doc1]
-		// }
-		fmt.Println(SimArray[index], doc1, doc2)
+		// if not the same cluster, merge and update cosine
+		cluster1 := doc_cluster[doc1]
+		cluster2 := doc_cluster[doc2]
+		if cluster1 != cluster2 {
+			cluster_count--
+			// merge cluster 2 to cluster 1
+			for cluster2_doc_index := range cluster_doc[cluster2] {
+				cluster2_doc := cluster_doc[cluster2][cluster2_doc_index]
+				cluster_doc[cluster1] = append(cluster_doc[cluster1], cluster2_doc)
+				doc_cluster[cluster2_doc] = cluster1
+			}
+			delete(cluster_doc, cluster2)
+		}
+		// fmt.Println(SimArray[index], doc1, doc2)
 	}
-	// showCluster(doc_cluster)
+	showCluster(cluster_doc)
 }
